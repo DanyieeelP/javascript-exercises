2225fd40451df509d22e445ddacf4a447777070f
DanyieeelP <Patriarcadaniel15@gmail.com>
2026-01-01 18:11:17 +0800

    Add variable and join strings

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,7 +8,23 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
+
+console.log("firstName is " + firstName);
+console.log("lastname is " + lastName);
+console.log("this year is " + thisYear);
+console.log("birth year is " + birthYear);
+console.log(greeting);
+
+console.log(fullName);
+console.log(age);
 
 
 
